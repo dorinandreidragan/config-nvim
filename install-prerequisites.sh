bbf7c32ce1e2fb436c6cefb098fc6ab7cedee3a2
Dorin Andrei Dragan <dorin.andrei.dragan@gmail.com>
2022-01-14 10:47:06 +0200

    Add auto confirmation for installations

--- install-prerequisites.sh
+++ install-prerequisites.sh
@@ -1,7 +1,9 @@
+set -e
+
 echo "Install neovim"
-sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
 sudo apt-get update
-sudo apt-get install neovim
+sudo apt-get install -y neovim
 
 echo "Install plug.vim"
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
@@ -15,7 +17,7 @@ echo "Install node"
 nvm install node
 
 echo "Install pip"
-sudo apt install python3-pip
+sudo apt install -y python3-pip
 
 echo "Install pynvim for Python plugins"
 python3 -m pip install --user --upgrade pynvim
